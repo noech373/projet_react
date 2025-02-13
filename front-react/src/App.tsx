53b7632f04bc33b527b8c74159b436878c8dea0f
Thomctrs <thomas.catros@isen-ouest.yncrea.fr>
2025-02-13 11:12:10 +0100

    update app

--- front-react/src/App.tsx
+++ front-react/src/App.tsx
@@ -1,14 +1,11 @@
-import {useEffect, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect} from 'react'
 import './App.css'
-import {useFetcher} from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import { Layout } from './components/layout/Layout';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
   useEffect(() => {
       fetch('http://localhost:3000/posts')
             .then(response => response.json())
