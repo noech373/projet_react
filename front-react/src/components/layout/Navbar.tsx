e7de97951d0de47f7dd1603910d4f9c926d409e0
Thomctrs <thomas.catros@isen-ouest.yncrea.fr>
2025-02-13 14:24:29 +0100

    add navbar

--- front-react/src/components/layout/Navbar.tsx
+++ front-react/src/components/layout/Navbar.tsx
@@ -6,11 +6,11 @@ export const Navbar = () => {
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <Link to="/" className="text-xl font-bold">
-            EventPlatform
+            EventPlatform, la plateforme de réservation d'événements
           </Link>
           <div className="flex space-x-4">
             <Link to="/" className="text-gray-600 hover:text-gray-900">
-              Accueil
+              Accueil     
             </Link>
             <Link to="/cart" className="text-gray-600 hover:text-gray-900">
               Panier
